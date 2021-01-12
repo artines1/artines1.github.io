960714d9e0b1c7d204941bcef0eb5f87c727dcb2
Tim Huang <tihuang@mozilla.com>
2021-01-12 15:10:35 +0100

    Add more SW tests

--- test/sw/sw.js
+++ test/sw/sw.js
@@ -1,5 +1,18 @@
 const CACHE_NAME = "Test_Cache";
 
+let sw_uuid = "";
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+self.addEventListener('install', (event) => {
+  sw_uuid = uuidv4();
+});
+
 function ResponseMsg(msg) {
   self.clients.matchAll({
     includeUncontrolled: true,
@@ -53,5 +66,11 @@ self.addEventListener('message', (event) => {
         ResponseMsg("The cache doesn't exist, so cannot be deleted");
       }
     });
+  }
+
+  if (event.data && event.data.type === 'GetUUID') {
+    console.log("Fetching the UUID in SW ...");
+
+    ResponseMsg(`SW UUID: ${sw_uuid}`);
   }
 });
